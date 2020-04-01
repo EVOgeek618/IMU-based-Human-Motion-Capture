fb67457b78853c633bf0491579c6076f4e578c90
artemoneto <47323236+artemoneto@users.noreply.github.com>
2020-04-01 17:54:26 +0300

    Update getData.py
    Добавил остановку скрипта по комбинации клавиш Ctrl+C (Del)

--- tracker/getData.py
+++ tracker/getData.py
@@ -5,8 +5,12 @@ import time
 ser = serial.Serial('/dev/serial/by-path/pci-0000:04:00.3-usb-0:3.4:1.0-port0', 1000000)
 T = time.CLOCK_REALTIME
 with open('getData.txt', 'w') as file:
-	while 1:
-		serial_line = str(ser.readline()) +' '+ str(time.clock_gettime(T))
-		file.write('\n'+serial_line)
+	try
+		while True:
+			serial_line = str(ser.readline()) +' '+ str(time.clock_gettime(T))
+			file.write('\n'+serial_line)
+	except KeyboardInterrupt:
+    		print("Press Ctrl-C to terminate while statement")
+    		pass
 
 	
