cf9304faa1134efdb62fc4b87228e98b63157685
artemoneto <47323236+artemoneto@users.noreply.github.com>
2020-05-06 10:11:21 +0300

    Update WiFi_msg.py

--- tracker/BNO080/WiFi_msg.py
+++ tracker/BNO080/WiFi_msg.py
@@ -15,14 +15,6 @@ s.listen(0)
 
 vector = (0, 0, 0)
 
-def handle_turtle_pose(msg, turtlename):
-    br = tf.TransformBroadcaster()
-    br.sendTransform((msg.x, msg.y, 0),
-                     tf.transformations.quaternion_from_euler(0, 0, msg.theta),
-                     rospy.Time.now(),
-                     turtlename,
-                     "world")
-
 if __name__ == '__main__':
 	while True:
 		client, addr = s.accept()
